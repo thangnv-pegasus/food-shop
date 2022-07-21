f2068e994f3c715a83887ee2cb591e345a1b9ab6
PlayBi <85682158+PlayBi@users.noreply.github.com>
2022-07-22 02:37:35 +0700

    Ngày 1. Tạo Dự Án

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import GlobalStyle from '~/component/GlobalStyle';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <App />
+    </GlobalStyle>
   </React.StrictMode>
 );
 
